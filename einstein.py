8718c24a1a7da73262a6d82966acadb6d3df1aef
Landon-Bragg <107891432+Landon-Bragg@users.noreply.github.com>
2022-07-06 14:24:17 -0500

    Add files via upload

---
+++ einstein.py
@@ -0,0 +1,18 @@
+#def main():
+   # kilos = int(input("How many kilos does your mama weigh? "))
+  #  resultjoules = conversion(kilos)
+ #   print(resultjoules, "joules")
+
+#def conversion(kilos):
+   # resultjoules = kilos * 90000000000000000
+  #  return(resultjoules)
+
+#main()
+
+M = int(input("m: "))
+
+c = 300000000
+
+E = M * (c ** 2)
+
+print(E)
